3b2373b522a9e19c289a1b1b5bf6263fa9657075
mirna taher <mirna.abdelazeem@gmail.com>
2022-06-27 00:55:06 +0200

    adding pagination

--- src/app/components/blog/blog-listing/blog-listing.component.ts
+++ src/app/components/blog/blog-listing/blog-listing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BlogService } from '../blog.service';
 import { BlogType } from './blog-type';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-blog-listing',
@@ -16,11 +17,31 @@ export class BlogListingComponent implements OnInit {
   ) { }
   bgColor: any = '#f5f7fa';
   blogList: BlogType[] = [];
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 9;
+  tableSizes: number[] = [9, 12, 15, 18];
+
   ngOnInit(): void {
+    this.getBlogList();
+  }
+  getBlogList(): void {
     this.blogService.getBlogs().subscribe((list) => {
       this.blogList = list;
     });
   }
+  // pagination
+  onTableDataChange(event: any): void {
+    this.page = event.target.value;
+    this.getBlogList();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.getBlogList();
+  }
+
+  // when click on card
   goToBlogDetails(blog: BlogType) {
     this.router.navigate(['/blog', blog.id]);
   }
